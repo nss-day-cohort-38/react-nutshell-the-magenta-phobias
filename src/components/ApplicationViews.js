307c25435be77b9616a11c0877e2c1e27f123ee5
Matt Crook <matt.crook11@gmail.com>
2020-03-05 16:38:11 -0600

    wip: applicationview routing update to include home and login.

--- src/components/ApplicationViews.js
+++ src/components/ApplicationViews.js
@@ -1,16 +1,34 @@
 import { Route, Redirect } from "react-router-dom";
-import React from "react";
-import Home from "./home/Home"
+import React, { Component } from "react";
+import Home from "./home/Home";
+import Login from "./auth/Login";
 
-const ApplicationViews = (props) => {
+const ApplicationViews = props => {
+  const hasUser = props.hasUser;
+  const setUser = props.setUser;
 
-    return(
-        <Route
+  return (
+    <React.Fragment>
+      <Route
+        exact
         path="/"
-        render={props=> (
-            <Home />
-        )}
-        />
-    )
-}
-export default ApplicationViews
+        render={props =>
+          hasUser ? (
+            <Home {...props} />
+          ) : (
+            <Redirect to="/login" component={Login} />
+          )
+        }
+      />
+
+      <Route
+        exact
+        path="/login"
+        render={props => {
+          return <Login setUser={setUser} {...props} />;
+        }}
+      />
+    </React.Fragment>
+  );
+};
+export default ApplicationViews;
