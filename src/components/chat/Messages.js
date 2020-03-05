6afb80e65e09a4220ae2e06a8716338fb3bece67
Keith Potempa <keithrpotempa@gmail.com>
2020-03-05 15:14:57 -0600

    starting on messages

---
+++ src/components/chat/Messages.js
@@ -0,0 +1,13 @@
+import React, { useState, useEffect } from 'react';
+import ApiManager from '../../modules/ApiManager'
+
+const MessageList = props => {
+  const [messages, setMessages] = useState([]);
+  const [messageOwners, setMessageOwners] = useState([]);
+
+  const getMessages = () => {
+    
+  }
+}
+
+export default MessageList
