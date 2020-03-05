8ceb50d86cee9d413658ecd6b02d510590977f86
Kurt Krafft <kkrafft6609@gmail.com>
2020-03-05 14:03:54 -0600

    header

--- src/Nutshell.js
+++ src/Nutshell.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 // import ApplicationViews from "./ApplicationViews";
 import NavBar from "./components/nav/NavBar"
 import "./NavBar.css";
+import Header from "./components/nav/Header"
 
 const Nutshell = () => {
     return (
         <>
         <NavBar />
+        <Header />
         {/* <ApplicationViews  /> */}
         </>
     )
---
+++ src/components/nav/Header.js
@@ -0,0 +1,13 @@
+import React, {useState, useEffect} from "react"
+
+const Header = props => {
+    return (
+        <>
+        <header>
+        <h1 className="title">fôl<i class="circle icon"></i></h1>
+        </header>
+        </>
+    )
+}
+
+export default Header;
