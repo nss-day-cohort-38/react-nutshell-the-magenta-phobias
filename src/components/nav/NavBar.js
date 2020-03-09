061ca118d80b1b6282a69b875cbed1995fdc5161
Matt Crook <matt.crook11@gmail.com>
2020-03-09 10:49:23 -0500

    Login form and auth/ session storage (#21)
    * feat: User sign in form created. Todo: Sign up form.
    
    * feat: login form routed with NavBar and put in nutshell compoenent.
    
    * wip:
    
    * wip: applicationview routing update to include home and login.
    
    * feat: sign in functionality. Session and local storage of user saves when logging in.
    
    * feat: Create Account feature functional. Both Login and create account work, needs styling and check so user cannot login without reating account first.
    
    * feat: Login and creat user account CSS styling.
    
    * feat: login auth functional. Create Account (user registration) inactive.
    
    * feat: Auth check for a 200 in fetch request of user login.
    
    * feat: Authentication for login. Checks user email and password matches what is in DB. USes GET which is NOT best practice, but works for uses of this project. Will throw error if user email and or password does not exsist.
    
    * fix: CSS selectors to fix specificity.
    
    * fix: CSS and added setCredentials to Login.js for session storage.
    
    * fix: setCredetials stores users full resource in session storage.
    
    * fix: wip commit. Users resource in session storage.

--- src/components/nav/NavBar.js
+++ src/components/nav/NavBar.js
@@ -1,31 +1,43 @@
-import React from "react";  
-import {slide as Menu} from "react-burger-menu"
+import React from "react";
+import { slide as Menu } from "react-burger-menu";
+import { NavLink } from "react-router-dom";
+import { Link } from "react-router-dom";
+import { withRouter } from "react-router-dom";
+import "./NavBar.css";
 
 const NavBar = props => {
-    return (
-        <Menu>
-        <a className = "menu-item" href="/">
-            Home
+  const handleLogout = () => {
+    props.clearUser();
+    props.history.push("/");
+  };
+  return (
+    <Menu>
+      <a className="menu-item" href="/">
+        {" "}
+        Home{""}
         </a>
-        <a className = "menu-item" href="/news">
-            News
-        </a>
-        <a className = "menu-item" href="/events">
-            Events
-        </a>
-        <a className = "menu-item" href="/tasks">
-            Tasks
-        </a>
-        <a className = "menu-item" href="/chat">
-            Chat
-        </a>
-        <a className = "menu-item" href="/friends">
-            Friends
-        </a>
-        <a className = "menu-item" href="/login">
-            Login
-        </a>
-        </Menu>
-    )
-}
-export default NavBar;
+      <a className="menu-item" href="/news">
+        News
+      </a>
+      <a className="menu-item" href="/events">
+        Events
+      </a>
+      <a className="menu-item" href="/tasks">
+        Tasks
+      </a>
+      <a className="menu-item" href="/chat">
+        Chat
+      </a>
+      <a className="menu-item" href="/friends">
+        Friends
+      </a>
+      {props.hasUser ? (
+          <span className="menu-item" onClick={handleLogout}>
+            {" "}
+            Logout{" "}
+          </span>
+      ) : null}
+    </Menu>
+  );
+};
+export default withRouter(NavBar);
